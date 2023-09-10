5523132946f4872b7f444705fe1ba0e821767d0e
Alexander Weigelt <webdesign@alexander-weigelt.de>
2023-09-10 15:08:49 +0200

    Add publishing methods

---
+++ packages/icon-library/dist/IconComponent.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function IconComponent() {
+    return 'Hello from iconLibray';
+}
+exports.default = IconComponent();
+//# sourceMappingURL=IconComponent.js.map
